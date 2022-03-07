a0ec185c06484299426a8120eabb4d99e39764cd
fmothe <mothevv.13@gmail.com>
2022-03-07 13:46:08 -0300

    feat(Root): calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "../ui/Navbar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../../styles/generalstyles.css";
+import { CalendarEvent } from "./CalendarEvent";
 
 export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('calendarView') || 'month')
+  const onDoubleClick = (e) =>{
+    console.log(e)
+  }
+  const onSelectEvent = (e) =>{
+    console.log(e)
+  }
+  const onViewChange = (e) =>{
+    setLastView(e)
+    localStorage.setItem("calendarView", e)
+  }
+
+  const eventStyleGetter = (event, start, end, isSelected) =>{
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display:'block',
+      color: 'white',
+    }
+    return {
+      style
+    }
+  }
+  const localizer = momentLocalizer(moment);
+  const eventsList = [
+    {
+      title: "Que miras",
+      start: moment().toDate(),
+      end: moment().add(2, "hours").toDate(),
+      bgColor: "#fafafa",
+      user: {
+        _id:'123',
+        name:'Federico'
+      }
+    },
+  ];
   return (
-    <>
-    <Navbar/>
-     <h1>CalendarScreen</h1>
-    </>
+    <div className="calendar-screen">
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={eventsList}
+        startAccessor="start"
+        endAccessor="end"
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{event: CalendarEvent}}
+      />
+    </div>
   );
 };
